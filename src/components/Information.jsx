91990a5a96e1a24a3d1d03d5130ccac124b5d63f
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2024-08-24 00:14:55 -0700

    Updates Information component

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,15 +1,27 @@
-//TODO: Rewrite instructions + make this toggle-able
+import { useState } from "react";
 
-export const Information = () => (
-  <>
-    <h1>Character Balancer</h1>
-    <p>
-      How to use:
-      <br />
-      For each pair of adjectives, evaluate where your character falls on the
-      scale and click on the appropriate button. Once you've completed your
-      evaluation, you will see a category-by-category & overall breakdown of how
-      balanced your character is.
-    </p>
-  </>
-);
+export const Information = () => {
+  const [isShown, setIsShown] = useState(true);
+
+  const handleClick = () => {
+    setIsShown(!isShown);
+  };
+
+  const instructions =
+    "For each pair of adjectives, evaluate where your character falls on the scale and click on the appropriate button. Once you've completed your evaluation, you will see a category-by-category & overall breakdown of how balanced your character is.";
+
+  return (
+    <div>
+      <h1>Character Balancer</h1>
+      {!isShown ? null : (
+        <p>
+          How to use:
+          <br /> {instructions}
+        </p>
+      )}
+      <button className="instructionsButton" onClick={handleClick}>
+        {isShown ? "Hide Instructions" : "Show Instructions"}
+      </button>
+    </div>
+  );
+};
